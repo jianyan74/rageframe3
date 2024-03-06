50c3d8e78f6963556463593fc9c42566054e93ee
jianyan <751393839@qq.com>
2024-03-06 15:38:40 +0800

    优化博客插件

--- addons/TinyBlog/Upgrade.php
+++ addons/TinyBlog/Upgrade.php
@@ -5,6 +5,8 @@ namespace addons\TinyBlog;
 use Yii;
 use common\components\Migration;
 use common\interfaces\AddonWidget;
+use common\helpers\StringHelper;
+use addons\TinyBlog\common\models\Cate;
 
 /**
  * 升级数据库
@@ -20,6 +22,7 @@ class Upgrade extends Migration implements AddonWidget
     public $versions = [
         '1.0.0', // 默认版本
         '1.0.1',
+        '1.0.2',
     ];
 
     /**
@@ -30,9 +33,31 @@ class Upgrade extends Migration implements AddonWidget
     public function run($addon)
     {
         switch ($addon->version) {
+            case '1.0.2' :
+                $models = Cate::find()->select(['id', 'tree'])->asArray()->all();
+                foreach ($models as $model) {
+                    $this->updateTree($model['id'], $model['tree'], Cate::class);
+                }
+                break;
             case '1.0.1' :
                 $this->addColumn('{{%addon_tiny_blog_tag}}', 'frequency', "int(10) NULL DEFAULT '0' COMMENT '使用次数'");
                 break;
+        }
+    }
+
+    /**
+     * @param int $id
+     * @param string $tree
+     * @param $model
+     * @return void
+     */
+    protected function updateTree($id, $tree, $model)
+    {
+        $tree = StringHelper::replace(' ', '', $tree);
+        $endTree = substr($tree, strlen($tree) - 1);
+        if ($endTree != '-') {
+            $tree = $tree.'-';
+            $model::updateAll(['tree' => $tree], ['id' => $id]);
         }
     }
 }
